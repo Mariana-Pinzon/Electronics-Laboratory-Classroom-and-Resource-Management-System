99687392260703dde4e75ba5e63cade4ef4faae8
Mariana-Pinzon <pinzonlimp@gmail.com>
2024-09-14 12:11:05 -0500

    add Users-Permissions

---
+++ Electronics Laboratory Classroom and Resource Management System/Model/User_Permission.cs
@@ -0,0 +1,9 @@
+﻿namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Model
+{
+    public class User_Permission
+    {
+        public int UserP_ID { get; set; }
+        public virtual required User_Type User_Type{ get; set; }
+        public virtual required Permission Permission { get; set; }
+    }
+}
