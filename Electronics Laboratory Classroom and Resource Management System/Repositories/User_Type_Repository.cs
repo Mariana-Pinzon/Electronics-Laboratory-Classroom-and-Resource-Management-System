9fe11c0c2ef1a8353d95560928767732731f2666
Mariana-Pinzon <pinzonlimp@gmail.com>
2024-09-21 12:13:09 -0500

    Changes User_Type_Repository

--- Electronics Laboratory Classroom and Resource Management System/Repositories/User_Type_Repository.cs
+++ Electronics Laboratory Classroom and Resource Management System/Repositories/User_Type_Repository.cs
@@ -1,6 +1,18 @@
-﻿namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Repository
+﻿using Electronics_Laboratory_Classroom_and_Resource_Management_System.Model;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Repository
 {
     public class User_Type_Repository
     {
+        public interface IUser_Type_Repository
+        {
+            Task<IEnumerable<User_Type>> GetAllUser_TypeAsync();
+            Task<User_Type> GetUser_TypeByIdAsync(int ID);
+            Task CreateUser_TypeAsync(User_Type user_Type);
+            Task UpdateUser_TypeAsync(User_Type user_Type);
+            Task SoftDeleteUser_TypeAsync(int ID);
+
+        }
     }
 }
