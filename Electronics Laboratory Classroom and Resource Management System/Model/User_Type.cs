8b95a1d70b89654f79587d6b1b408ac6dec45faa
Mariana-Pinzon <pinzonlimp@gmail.com>
2024-09-22 18:17:36 -0500

    Repository User_Type

--- Electronics Laboratory Classroom and Resource Management System/Model/User_Type.cs
+++ Electronics Laboratory Classroom and Resource Management System/Model/User_Type.cs
@@ -6,5 +6,6 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Model
     {
         public int User_Type_ID { get; set; }
         public required string UserType { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
