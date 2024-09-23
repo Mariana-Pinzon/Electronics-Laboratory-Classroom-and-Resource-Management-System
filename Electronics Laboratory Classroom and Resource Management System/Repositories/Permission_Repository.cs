b0b56dbc85be73ef1c6743cf06ab754ad8b80c87
Mariana-Pinzon <pinzonlimp@gmail.com>
2024-09-23 15:55:09 -0500

    Created Permission_Repository, Permission_Controller and PermissionService

---
+++ Electronics Laboratory Classroom and Resource Management System/Repositories/Permission_Repository.cs
@@ -0,0 +1,58 @@
+﻿using Electronics_Laboratory_Classroom_and_Resource_Management_System.Context;
+using Electronics_Laboratory_Classroom_and_Resource_Management_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Repositories
+{
+    public interface IPermission_Repository
+    {
+        Task<IEnumerable<Permission>> GetAllPermissionAsync();
+        Task<Permission> GetPermissionByIdAsync(int id);
+        Task CreatePermissionAsync(Permission permission);
+        Task UpdatePermissionAsync(Permission permission);
+        Task SoftDeletePermissionAsync(int id);
+    }
+    public class Permission_Repository : IPermission_Repository
+    {
+        private readonly ElectronicsLaboratoryClassroomandResourceDBContext _context;
+        public Permission_Repository(ElectronicsLaboratoryClassroomandResourceDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Permission>> GetAllPermissionAsync()
+        {
+            return await _context.permissions
+                .Where(p => !p.IsDeleted)
+                .ToListAsync();
+        }
+        public async Task<Permission> GetPermissionByIdAsync(int id)
+        {
+            return await _context.permissions
+                .FirstOrDefaultAsync(p => p.Permission_ID == id && !p.IsDeleted);
+        }
+
+        public async Task SoftDeletePermissionAsync(int id)
+        {
+            var permission = await _context.permissions.FindAsync(id);
+            if (permission != null)
+            {
+                permission.IsDeleted = true;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        // Implementación del método para crear un nuevo tipo de usuario
+        public async Task CreatePermissionAsync(Permission permission)
+        {
+            _context.permissions.Add(permission);
+            await _context.SaveChangesAsync();
+        }
+
+        // Implementación del método para actualizar un tipo de usuario
+        public async Task UpdatePermissionAsync(Permission permission)
+        {
+            _context.permissions.Update(permission);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Electronics Laboratory Classroom and Resource Management System/Services/PermissionService.cs
@@ -0,0 +1,49 @@
+﻿using Electronics_Laboratory_Classroom_and_Resource_Management_System.Model;
+using Electronics_Laboratory_Classroom_and_Resource_Management_System.Repositories;
+using Electronics_Laboratory_Classroom_and_Resource_Management_System.Repository;
+
+namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Services
+{
+    public interface IPermissionService
+    {
+        Task<IEnumerable<Permission>> GetAllPermissionAsync();
+        Task<Permission> GetPermissionByIdAsync(int id);
+        Task CreatePermissionAsync(Permission permission);
+        Task UpdatePermissionAsync(Permission permission);
+        Task SoftDeletePermissionAsync(int id);
+    }
+    public class PermissionService : IPermissionService
+    {
+        private readonly IPermission_Repository _permissionRepository;
+
+        public PermissionService(IPermission_Repository permissionRepository)
+        {
+            _permissionRepository = permissionRepository;
+        }
+
+        public async Task<IEnumerable<Permission>> GetAllPermissionAsync()
+        {
+            return await _permissionRepository.GetAllPermissionAsync();
+        }
+
+        public async Task<Permission> GetPermissionByIdAsync(int Permission_ID)
+        {
+            return await _permissionRepository.GetPermissionByIdAsync(Permission_ID);
+        }
+
+        public async Task CreatePermissionAsync(Permission permission)
+        {
+            await _permissionRepository.CreatePermissionAsync(permission);
+        }
+
+        public async Task UpdatePermissionAsync(Permission permission)
+        {
+            await _permissionRepository.UpdatePermissionAsync(permission);
+        }
+
+        public async Task SoftDeletePermissionAsync(int Permission_ID)
+        {
+            await _permissionRepository.SoftDeletePermissionAsync(Permission_ID);
+        }
+    }
+}
