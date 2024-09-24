85d727a2db2022557eb27e94b1f70c0c201e3214
Daniel071109 <kenirex123@gmail.com>
2024-09-23 22:53:34 -0500

    Created Reservation_Controller,Reservation_Repository and ReservationService

---
+++ Electronics Laboratory Classroom and Resource Management System/Repositories/Reservation_Repository.cs
@@ -0,0 +1,57 @@
+﻿using Electronics_Laboratory_Classroom_and_Resource_Management_System.Context;
+using Electronics_Laboratory_Classroom_and_Resource_Management_System.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Repositories
+{
+    public interface IReservation_Repository
+    {
+        Task<IEnumerable<Reservation>> GetAllReservationAsync();
+        Task<Reservation> GetReservationByIdAsync(int id);
+        Task CreateReservationAsync(Reservation reservation);
+        Task UpdateReservationAsync(Reservation reservation);
+        Task SoftDeleteReservationAsync(int id);
+    }
+    public class Reservation_Repository : IReservation_Repository
+    {
+        private readonly ElectronicsLaboratoryClassroomandResourceDBContext _context;
+        public Reservation_Repository(ElectronicsLaboratoryClassroomandResourceDBContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Reservation>> GetAllReservationAsync()
+        {
+            return await _context.reservations
+                .Where(r => !r.IsDeleted)
+                .ToListAsync();
+        }
+        public async Task<Reservation> GetReservationByIdAsync(int id)
+        {
+            return await _context.reservations
+                .FirstOrDefaultAsync(r => r.Reservation_ID == id && !r.IsDeleted);
+        }
+
+        public async Task SoftDeleteReservationAsync(int id)
+        {
+            var reservation = await _context.reservations.FindAsync(id);
+            if (reservation != null)
+            {
+                reservation.IsDeleted = true;
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task CreateReservationAsync(Reservation reservation)
+        {
+            _context.reservations.Add(reservation);
+            await _context.SaveChangesAsync();
+        }
+
+
+        public async Task UpdateReservationAsync(Reservation reservation)
+        {
+            _context.reservations.Update(reservation);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
