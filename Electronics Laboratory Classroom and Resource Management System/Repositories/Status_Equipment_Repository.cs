97e73adc16c5d903623e73e9ba0c0ab6f2cf376e
Daniel071109 <kenirex123@gmail.com>
2024-09-24 00:08:09 -0500

    Changes in Status_Equipment_Repository

--- Electronics Laboratory Classroom and Resource Management System/Repositories/Status_Equipment_Repository.cs
+++ Electronics Laboratory Classroom and Resource Management System/Repositories/Status_Equipment_Repository.cs
@@ -13,49 +13,50 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Reposi
         Task SoftDeleteStatus_EquipmentAsync(int id);
         public class Status_Equipment_Repository : IStatus_Equipment_Repository
         {
-            public class Status_Equipment_Repository : IStatus_Equipment_Repository
+            private readonly ElectronicsLaboratoryClassroomandResourceDBContext _context;
+            public Status_Equipment_Repository(ElectronicsLaboratoryClassroomandResourceDBContext context)
             {
-                private readonly ElectronicsLaboratoryClassroomandResourceDBContext _context;
-                public Status_Equipment_Repository(ElectronicsLaboratoryClassroomandResourceDBContext context)
-                {
-                    _context = context;
-                }
-                public async Task<IEnumerable<Status_Equipment>> GetAllStatus_EquipmentAsync()
-                {
-                    return await _context.status_equipments
-                        .Where(se => !se.IsDeleted)
-                        .ToListAsync();
-                }
-                public async Task<Status_Equipment> GetStatus_EquipmentByIdAsync(int id)
-                {
-                    return await _context.status_equipments
-                        .FirstOrDefaultAsync(se => se.StatusE_ID == id && !se.IsDeleted);
-                }
+                _context = context;
+            }
+            public async Task<IEnumerable<Status_Equipment>> GetAllStatus_EquipmentAsync()
+            {
+                return await _context.status_equipments
+                    .Where(e => !e.IsDeleted)
+                    .ToListAsync();
+            }
+            public async Task<Status_Equipment> GetStatus_EquipmentByIdAsync(int id)
+            {
+                return await _context.status_equipments
+                    .FirstOrDefaultAsync(se => se.StatusE_ID == id && !se.IsDeleted);
+            }
 
-                public async Task SoftDeleteStatus_EquipmentAsync(int id)
+            public async Task SoftDeleteStatus_EquipmentAsync(int id)
+            {
+                var status_equipment = await _context.status_equipments.FindAsync(id);
+                if (status_equipment != null)
                 {
-                    var status_equipment = await _context.status_equipments.FindAsync(id);
-                    if (status_equipment != null)
-                    {
-                        status_equipment.IsDeleted = true;
-                        await _context.SaveChangesAsync();
-                    }
-                }
-
-
-                public async Task CreateStatus_EquipmentAsync(Status_Equipment status_equipment)
-                {
-                    _context.status_equipments.Add(status_equipment);
+                    status_equipment.IsDeleted = true;
                     await _context.SaveChangesAsync();
                 }
+            }
 
-                public async Task UpdateStatus_EquipmentAsync(Status_Equipment status_equipment)
-                {
-                    _context.status_equipments.Update(status_equipment);
-                    await _context.SaveChangesAsync();
-                }
+            public async Task CreateStatus_EquipmentsAsync(Status_Equipment status_equipment)
+            {
+                _context.status_equipments.Add(status_equipment);
+                await _context.SaveChangesAsync();
+            }
+
+
+            public async Task UpdateStatus_EquipmentAsync(Status_Equipment status_equipment)
+            {
+                _context.status_equipments.Update(status_equipment);
+                await _context.SaveChangesAsync();
+            }
+
+            public Task CreateStatus_EquipmentAsync(Status_Equipment status_equipment)
+            {
+                throw new NotImplementedException();
             }
         }
     }
 }
-
