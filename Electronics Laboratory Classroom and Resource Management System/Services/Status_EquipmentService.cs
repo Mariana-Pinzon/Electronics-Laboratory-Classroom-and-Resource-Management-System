389e3c6e09c1d4d19a93226f4d1deb21763f0fb0
Daniel071109 <kenirex123@gmail.com>
2024-09-30 16:28:29 -0500

    Change in Status_Equipment

--- Electronics Laboratory Classroom and Resource Management System/Services/Status_EquipmentService.cs
+++ Electronics Laboratory Classroom and Resource Management System/Services/Status_EquipmentService.cs
@@ -7,42 +7,42 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Servic
     {
         Task<IEnumerable<Status_Equipment>> GetAllstatus_equipmentsAsync();
         Task<Status_Equipment> GetStatus_EquipmentByIdAsync(int id);
+        Task CreateStatus_EquipmentAsync(Status_Equipment status_Equipment);
+        Task UpdateStatus_EquipmentAsync(Status_Equipment status_Equipment);
         Task SoftDeleteStatus_EquipmentAsync(int id);
-        Task CreateStatus_EquipmentAsync(Model.Status_Equipment status_equipment);
-        Task UpdateStatus_EquipmentAsync(Model.Status_Equipment status_equipment);
-
     }
     public class Status_EquipmentService : IStatus_EquipmentService
     {
+        private readonly IStatus_Equipment_Repository _status_equipmentRepository;
 
+        public Status_EquipmentService(IStatus_Equipment_Repository status_equipmentRepository)
         {
             _status_equipmentRepository = status_equipmentRepository;
         }
 
+        public async Task<IEnumerable<Status_Equipment>> GetAllstatus_equipmentsAsync()
         {
+            return await _status_equipmentRepository.GetAllstatus_equipmentsAsync();
         }
 
+        public async Task<Status_Equipment> GetStatus_EquipmentByIdAsync(int id)
         {
+            return await _status_equipmentRepository.GetStatus_EquipmentByIdAsync(id);
         }
 
         public async Task CreateStatus_EquipmentAsync(Status_Equipment status_equipment)
         {
+            await _status_equipmentRepository.CreateStatus_EquipmentAsync(status_equipment);
         }
 
         public async Task UpdateStatus_EquipmentAsync(Status_Equipment status_equipment)
         {
+            await _status_equipmentRepository.UpdateStatus_EquipmentAsync(status_equipment);
         }
 
+        public async Task SoftDeleteStatus_EquipmentAsync(int id)
         {
+            await _status_equipmentRepository.SoftDeleteStatus_EquipmentAsync(id);
         }
-    }
-
-    public interface IStatus_Equipment
-    {
-        Task Createstatus_equipmentAsync(Status_Equipment status_equipment);
-        Task<IEnumerable<Status_Equipment>> GetAllStatus_EquipmentAsync();
-        Task<Status_Equipment> GetStatus_EquipmentByIdAsync(int statusE_ID);
-        Task SoftDeletestatus_equipmentAsync(int statusE_ID);
-        Task UpdateequipmentAsync(Status_Equipment status_equipment);
     }
 }
