5475125cfa8a5042b6779d4ec227de0a1dcd227f
Mariana-Pinzon <pinzonlimp@gmail.com>
2024-10-01 22:45:39 -0500

    Changes in Program

--- Electronics Laboratory Classroom and Resource Management System/Program.cs
+++ Electronics Laboratory Classroom and Resource Management System/Program.cs
@@ -19,7 +19,6 @@ internal class Program
         builder.Services.AddScoped<ILaboratory_Repository, Laboratory_Repository>();
         builder.Services.AddScoped<IPermission_Repository, Permission_Repository>();
         builder.Services.AddScoped<IReservation_Equipment_Repository, Reservation_Equipment_Repository>();
-        builder.Services.AddScoped<IReservation_History_Repository, Reservation_History_Repository>();
         builder.Services.AddScoped<IReservation_Repository, Reservation_Repository>();
         builder.Services.AddScoped<IStatus_Equipment_Repository, Status_Equipment_Repository>();
         builder.Services.AddScoped<IStatus_Reservation_Repository, Status_Reservation_Repository>();
@@ -27,6 +26,7 @@ internal class Program
         builder.Services.AddScoped<IUser_Repository, User_Repository>();
         builder.Services.AddScoped<IUser_Type_Repository, User_Type_Repository>();
         builder.Services.AddScoped<IUser_History_Repository, User_History_Repository>();
+        builder.Services.AddScoped<IReservation_History_Repository, Reservation_History_Repository>();
         builder.Services.AddScoped<IInventory_History_Repository, Inventory_History_Repository>();
         #endregion
         #region Services
@@ -35,7 +35,6 @@ internal class Program
         builder.Services.AddScoped<ILaboratoryService, LaboratoryService>();
         builder.Services.AddScoped<IPermissionService, PermissionService>();
         builder.Services.AddScoped<IReservation_EquipmentService, Reservation_EquipmentService>();
-        builder.Services.AddScoped<IReservation_HistoryService, Reservation_HistoryService>();
         builder.Services.AddScoped<IReservationService, ReservationService>();
         builder.Services.AddScoped<IStatus_EquipmentService, Status_EquipmentService>();
         builder.Services.AddScoped<IStatus_ReservationService, Status_ReservationService>();
@@ -43,6 +42,7 @@ internal class Program
         builder.Services.AddScoped<IUserService, UserService>();
         builder.Services.AddScoped<IUser_TypeService, User_TypeService>();
         builder.Services.AddScoped<IUser_HistoryService, User_HistoryService>();
+        builder.Services.AddScoped<IReservation_HistoryService, Reservation_HistoryService>();
         builder.Services.AddScoped<IInventory_HistoryService, Inventory_HistoryService>();
         #endregion
 
