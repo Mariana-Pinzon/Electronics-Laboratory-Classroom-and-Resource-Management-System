24f1708754a307fa4e38dcc6130d42bbfa50f0bb
Daniel071109 <kenirex123@gmail.com>
2024-10-04 20:08:32 -0500

    Creation of Status_Reservation trigger and User trigger

--- Electronics Laboratory Classroom and Resource Management System/Migrations/20241004184941_Void.cs
+++ Electronics Laboratory Classroom and Resource Management System/Migrations/20241004184941_Void.cs
@@ -13,7 +13,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
             /// <inheritdoc />
             /// Trigger Inventory
             migrationBuilder.Sql(@"
-     CREATE OR ALTER TRIGGER trg_AuditStudent
+     CREATE OR ALTER TRIGGER trg_Inventory
      ON[inventories]
 
      AFTER INSERT, UPDATE, DELETE
@@ -45,7 +45,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
 
             //Trigger Status_Equipment
             migrationBuilder.Sql(@"
-     CREATE OR ALTER TRIGGER trg_AuditAttendant
+     CREATE OR ALTER TRIGGER trg_Status_Equipment
      ON [status_equipments]
 
      AFTER INSERT, UPDATE, DELETE
@@ -73,6 +73,70 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
                    FROM deleted d;
          END
      END;");
+
+            //Trigger Status_Reservation
+            migrationBuilder.Sql(@"
+     CREATE OR ALTER TRIGGER trg_Status_Reservation
+     ON [status_reservations]
+
+     AFTER INSERT, UPDATE, DELETE
+     AS
+     BEGIN
+         SET NOCOUNT ON;
+         -- If there are inserted or updated records
+         IF EXISTS (SELECT * FROM inserted)
+         BEGIN
+             INSERT INTO Status_Reservation_History (Status_Reservation_History_ID,StatusR_ID, StatusR, Date, ModifiedDate, ModifiedBy)
+             SELECT i.Id, i.StatusR_ID, i.StatusR, i. Date, i.ModifiedDate, i. ModifiedBy,
+                 GETDATE(),
+                    CASE 
+                        WHEN EXISTS (SELECT * FROM deleted) THEN 'UPDATE' 
+                        ELSE 'INSERT' 
+                    END
+             FROM inserted i;
+         END
+ 
+         -- If there are deleted records
+         IF EXISTS (SELECT * FROM deleted)
+         BEGIN
+             INSERT INTO Status_Reservation_History (Status_Reservation_History_ID,StatusR_ID, StatusR, Date, ModifiedDate, ModifiedBy)
+             SELECT d.Id, d.StatusR_ID, d.StatusR, d.Date, d.ModifiedDate, d.ModifiedBy GETDATE(), 'DELETE'
+                   FROM deleted d;
+         END
+     END;");
+
+            //Trigger User
+            migrationBuilder.Sql(@"
+     CREATE OR ALTER TRIGGER trg_User
+     ON [users]
+
+     AFTER INSERT, UPDATE, DELETE
+     AS
+     BEGIN
+         SET NOCOUNT ON;
+         -- If there are inserted or updated records
+         IF EXISTS (SELECT * FROM inserted)
+         BEGIN
+             INSERT INTO User_History (User_History_ID,User_ID, First_Name, Last_Name, Email, Password, User_Type_ID, Date, ModifiedDate, ModifiedBy)
+             SELECT i.Id, i.User_ID, i.First_Name, i.Last_Name, i.Email, i. Password, i.User_Type_ID,  i. Date, i.ModifiedDate, i.ModifiedBy,
+                 GETDATE(),
+                    CASE 
+                        WHEN EXISTS (SELECT * FROM deleted) THEN 'UPDATE' 
+                        ELSE 'INSERT' 
+                    END
+             FROM inserted i;
+         END
+ 
+         -- If there are deleted records
+         IF EXISTS (SELECT * FROM deleted)
+         BEGIN
+             INSERT INTO User_History (User_History_ID,User_ID, First_Name, Last_Name, Email, Password, User_Type_ID, Date, ModifiedDate, ModifiedBy)
+             SELECT d.Id, d.User_ID, d.First_Name, d.Last_Name, d.Email, d.Password, d.User_Type_ID, d.Date, d.ModifiedDate, d.ModifiedBy GETDATE(), 'DELETE'
+                   FROM deleted d;
+         END
+     END;");
+
+
         }
 
     /// <inheritdoc />
