aef9ae22502c47280f619b45bb5f169ea07de650
Juan Sebastian Garzon Gomez <kenirex123@gmail.com>
2024-10-04 20:16:17 -0500

    Modiefied the Inventory,Status_Equipment,Status_Reservation and Users

--- Electronics Laboratory Classroom and Resource Management System/Migrations/20241004184941_Void.cs
+++ Electronics Laboratory Classroom and Resource Management System/Migrations/20241004184941_Void.cs
@@ -37,7 +37,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
      IF EXISTS(SELECT * FROM deleted)
          BEGIN
              INSERT INTO Inventory_History(Inventory_History_ID,Inventory_ID,Equipment_ID,Available_quantity,Total_quantity,Last_update,Laboratory_ID,Date,ModifiedDate, ModifiedBy)
-             SELECT  d.Id, d.Inventory.ID, d.Equipment.ID, d.Available_quantity, d.Total_quantity, d.Last_update, d.Laboratory_ID, d.Date,, GETDATE(), 'DELETE'
+             SELECT  d.Id, d.Inventory.ID, d.Equipment.ID, d.Available_quantity, d.Total_quantity, d.Last_update, d.Laboratory_ID, d.Date, GETDATE(), 'DELETE'
                    FROM deleted d;
      END
  END;
@@ -69,7 +69,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
          IF EXISTS (SELECT * FROM deleted)
          BEGIN
              INSERT INTO Status_Equipment_History (Status_Equipment_History_ID,StatusE_ID, Status, Date, ModifiedDate, ModifiedBy)
-             SELECT d.Id, d.StatusE_ID, d.Status, d.Date, d.ModifiedDate, d.ModifiedBy GETDATE(), 'DELETE'
+             SELECT d.Id, d.StatusE_ID, d.Status, d.Date, d.ModifiedDate, d.ModifiedBy, GETDATE(), 'DELETE'
                    FROM deleted d;
          END
      END;");
@@ -100,7 +100,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
          IF EXISTS (SELECT * FROM deleted)
          BEGIN
              INSERT INTO Status_Reservation_History (Status_Reservation_History_ID,StatusR_ID, StatusR, Date, ModifiedDate, ModifiedBy)
-             SELECT d.Id, d.StatusR_ID, d.StatusR, d.Date, d.ModifiedDate, d.ModifiedBy GETDATE(), 'DELETE'
+             SELECT d.Id, d.StatusR_ID, d.StatusR, d.Date, d.ModifiedDate, d.ModifiedBy, GETDATE(), 'DELETE'
                    FROM deleted d;
          END
      END;");
@@ -131,7 +131,7 @@ namespace Electronics_Laboratory_Classroom_and_Resource_Management_System.Migrat
          IF EXISTS (SELECT * FROM deleted)
          BEGIN
              INSERT INTO User_History (User_History_ID,User_ID, First_Name, Last_Name, Email, Password, User_Type_ID, Date, ModifiedDate, ModifiedBy)
-             SELECT d.Id, d.User_ID, d.First_Name, d.Last_Name, d.Email, d.Password, d.User_Type_ID, d.Date, d.ModifiedDate, d.ModifiedBy GETDATE(), 'DELETE'
+             SELECT d.Id, d.User_ID, d.First_Name, d.Last_Name, d.Email, d.Password, d.User_Type_ID, d.Date, d.ModifiedDate, d.ModifiedBy, GETDATE(), 'DELETE'
                    FROM deleted d;
          END
      END;");
